1468363adaf9b93ad6dc0505801f5cf4859cc098
Aliaksei Kalinkou <aliaksei.kalinkou@gmail.com>
2018-12-19 20:09:08 -0500

    Infrastructure

---
+++ src/main/java/com/akalinkou/busypostman/Starter.java
@@ -0,0 +1,8 @@
+package com.akalinkou.busypostman;
+
+public class Starter {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
---
+++ src/test/java/com/akalinkou/busypostman/DummyTest.java
@@ -0,0 +1,17 @@
+package com.akalinkou.busypostman;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class DummyTest {
+
+    @Test
+    public void test() {
+        Assert.assertTrue(true);
+    }
+
+    @Test
+    public void test2() {
+        Assert.assertTrue(false);
+    }
+}
